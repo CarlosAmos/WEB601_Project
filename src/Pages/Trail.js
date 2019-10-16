201a199edf0a35bd9fa6a3d6c9eb3affa527c542
CarlosAmos <carlos.amos1@gmail.com>
2019-10-16 20:26:34 +1300

    Added Trail Page

--- src/Pages/Trail.js
+++ src/Pages/Trail.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Home1 from '../Images/Home1.jpg';
+import { Container, Col, Row, Button } from 'react-bootstrap';
+
+const Styles = styled.div`
+.image{
+    width: 100%;
+    height: 100;
+    position: absolute;
+    border: '4px solid black' ;
+}
+.ContentBoxes{
+    border: '4px solid black' ;
+}
+
+}
+
+`;
 
 export const Trail = () => (
-    <div>
-        <h1>Trail Page</h1>
-        <p>This is a test</p>
+    <Container>
+    <div className="col-12">
+    <Row height='150'>
+        <div height='150px'>
+        <Button variant="outline-success">Success</Button>
+        </div>
+        </Row>
+        <Row className="justify-content-md-center" height='100'>
+        <Col xs="12" md="4"> 
+        <img src={Home1} alt="sefeee" width="100%"></img>
+            <h2>Trail 1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </Col>
+        <Col xs="12" md="4">
+        <img src={Home1} alt="sefeee" width="100%"></img>
+            <h2>Trail 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </Col>
+        <Col xs="12" md="4">
+        <img src={Home1} alt="sefeee" width="100%"></img>
+            <h2>Trail 3</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </Col>
+                     
+        </Row>      
+        <Row className="justify-content-md-center" height='100'>
+        <Col xs="12" md="4">
+        <img src={Home1} alt="sefeee" width="100%"></img>
+            <h2>Trail 4</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </Col>
+        <Col xs="12" md="4">
+        <img src={Home1} alt="sefeee" width="100%"></img>
+            <h2>Trail 5</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </Col>
+        <Col xs="12" md="4">
+        <img src={Home1} alt="sefeee" width="100%"></img>
+            <h2>Trail 6</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </Col>
+        </Row>       
+
     </div>
+    </Container>
 )
